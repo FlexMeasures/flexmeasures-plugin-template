68de861d12771af6a25bd07afa7c00f9db066e64
Nicolas Höning <iam@nicolashoening.de>
2021-10-26 17:21:38 +0200

    add tests which interact with the actual code. (#6)
    * add tests which interact with the actual code.
    
    * more stable FM version as default
    
    * better name and a cli_group for the CLI blueprint
    
    * fix module path for the re-import utility
    
    * fix typo

--- {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/cli/tests/test_cli.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/cli/tests/test_cli.py
@@ -1,2 +1,11 @@
-def test_dummy():
-    assert 1 == 1
+from ..commands import hello_world
+
+"""
+Useful resource: https://flask.palletsprojects.com/en/2.0.x/testing/#testing-cli-commands
+"""
+
+
+def test_hello_world(app):
+    runner = app.test_cli_runner()
+    result = runner.invoke(hello_world, ["--name", "George"])
+    assert "Hello, George!" in result.output
--- {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/ui/tests/test_views.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/ui/tests/test_views.py
@@ -3,7 +3,7 @@ from flask import url_for
 
 def test_view_responds(client):
     dashboard = client.get(
-        url_for("{{cookiecutter.module_name}}_ui_bp.dashboard"), follow_redirects=True
+        url_for("{{cookiecutter.plugin_name}} UI.dashboard"), follow_redirects=True
     )
     assert dashboard.status_code == 200
- 
+
