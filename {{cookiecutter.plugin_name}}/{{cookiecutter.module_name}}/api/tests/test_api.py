adbfa3990684544de84040cc2d983a5f5acd9162
Nicolas Höning <nicolas@seita.nl>
2021-10-26 16:20:44 +0200

    fix typo

--- {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/api/tests/test_api.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/api/tests/test_api.py
@@ -3,7 +3,7 @@ from flask import url_for
 
 def test_get_somedata_needs_login(client):
     response = client.get(url_for("{{cookiecutter.plugin_name}} API.somedata"), follow_redirects=True)
-    # TODO: this test requires auuthentication. Future work:
+    # TODO: this test requires authentication. Future work:
     # a) FM gives back HTML now, but after https://github.com/SeitaBV/flexmeasures/pull/210 it should be JSON
     # b) we should add a user, which also requires us to add a db to testing
     assert response.status_code == 200  # should be 401
