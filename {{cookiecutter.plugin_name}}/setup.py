e5c220eabd73764f73c84e21626ea15a0401ceed
Felix Claessen <30658763+Flix6x@users.noreply.github.com>
2023-09-19 14:30:43 +0200

    Update setup.py
    Streamline classifiers with FlexMeasures core.
    
    Signed-off-by: Felix Claessen <30658763+Flix6x@users.noreply.github.com>

--- {{cookiecutter.plugin_name}}/setup.py
+++ {{cookiecutter.plugin_name}}/setup.py
@@ -37,6 +37,7 @@ setup(
     include_package_data=True,  # setuptools_scm takes care of adding the files in SCM
     classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
