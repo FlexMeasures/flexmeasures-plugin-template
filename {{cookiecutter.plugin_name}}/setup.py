241b0f6fc95b41f7321f88369ef8c521e9aa3280
Felix Claessen <30658763+Flix6x@users.noreply.github.com>
2025-01-14 14:00:54 +0100

    Update Python versions anno 2025 (#26)
    * Add classifier for Python 3.12
    
    Signed-off-by: Felix Claessen <30658763+Flix6x@users.noreply.github.com>
    
    * feat: Suggest testing with Python 3.11 by default
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    ---------
    
    Signed-off-by: Felix Claessen <30658763+Flix6x@users.noreply.github.com>
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- {{cookiecutter.plugin_name}}/setup.py
+++ {{cookiecutter.plugin_name}}/setup.py
@@ -42,6 +42,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
